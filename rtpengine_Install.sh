8e4e0b68141628484a28161e278b32bd71456227
Binan <binanalhalabi@yahoo.com>
2015-11-21 12:58:45 +0100

    Add some variables

--- rtpengine_Install.sh
+++ rtpengine_Install.sh
@@ -1,8 +1,14 @@
+#! /bin/sh
+
 # Bash Script To Compile and Install SIPWise RTPEngine On RedHat Based Systems
 # Tested on Fedora 23, x86_64, Kernel: 4.2.6-3
 # Author: Binan AL Halabi
 
-#! /bin/sh
+src="/usr/local/src/"
+rtpengineSRC="$src/rtpengine"
+kernelBuild="/usr/lib/modules/$(uname -r)/build/"
+extraModules="/lib/modules/$(uname -r)/extra"
+xtablesModules="/lib64/xtables"
 
 # Install Some Packages
 dnf -y install git glib2 glib2-devel gcc zlib zlib-devel openssl openssl-devel pcre pcre-devel libcurl libcurl-devel xmlrpc-c xmlrpc-c-devel
@@ -10,34 +16,33 @@ dnf -y install git glib2 glib2-devel gcc zlib zlib-devel openssl openssl-devel p
 dnf -y install hiredis hiredis-devel
 
 
-# Get the RTPEngine Source
-cd /usr/local/src/
-git clone https://github.com/sipwise/rtpengine.git
+# Get the RTPEngine Source In The Folder $src
+git clone https://github.com/sipwise/rtpengine.git $src
 
 # Compile the daemon
-cd rtpengine/daemon;make
+cd "$rtpengineSRC/daemon";make
 
-# Install the daemon
+# Install the daemon, No "install" rule in th Makefile
 cp rtpengine /usr/sbin/rtpengine
 
 # Install the header files of the iptables
 dnf -y install iptables-devel
+
 # Compile and Install The iptables extension "libxt_RTPENGINE"
-cd /usr/local/src/rtpengine/iptables-extension;make
+cd "$rtpengineSRC/iptables-extension";make
 # Install the extension
-cp libxt_RTPENGINE.so /lib64/xtables
+cp libxt_RTPENGINE.so "$xtablesModules"
 
 # Install the kernel header files
 dnf -y install kernel-devel-$(uname -r)
-kernelD="/usr/lib/modules/$(uname -r)/build/"
-if [ ! -d $kernelD ]; then
+if [ ! -d $kernelBuild ]; then
 	echo "Could Not Find the Kernel Headers For The current Kernel.\n"
 	exit
 fi
 
 # Compile and Install the kernel module "xt_RTPENGINE"
-cd /usr/local/src/rtpengine/kernel-module;make
-cp xt_RTPENGINE.ko "/lib/modules/$(uname -r)/extra"
+cd "$rtpengineSRC/kernel-module";make
+cp xt_RTPENGINE.ko $extraModules
 
 # Create a list of modules dependencies
 depmod  -a
